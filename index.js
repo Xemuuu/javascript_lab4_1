10214a4622a5843a63ad0ef01cb9210538887cfd
Dawid <dawid.kow11@gmail.com>
2025-11-25 09:12:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// define endpoint for exercise 1 here
+app.get('/math/circle/:r', (req, res) => {
+  const r = parseFloat(req.params.r);
+  const area = (Math.PI * r * r).toFixed(2);
+  const circumference = (2 * Math.PI * r).toFixed(2);
+  res.json({ area, circumference });
+});
+
+//TODO2
+
+
+//TODO3
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
